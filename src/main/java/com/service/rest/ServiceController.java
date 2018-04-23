937f1b20e12d45e849a305f25f089cf6fb547b25
Arjun <arjunsekar1991@gmail.com>
2018-04-24 01:05:29 +0530

    rest sample services
    rest sample services

--- src/main/java/com/service/rest/ServiceController.java
+++ src/main/java/com/service/rest/ServiceController.java
@@ -1,12 +1,22 @@
 package com.service.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class ServiceController {
-	 @GetMapping("/hello")
-	    public String hello() {
-	        return "helloworld";
-	    }
+
+	@GetMapping(value = "/hello")
+	public String hello() {
+		return "helloworld";
+	}
+
+	@PostMapping(value = "/data", produces = { "application/xml", "application/json" }, consumes = { "application/xml",
+			"application/json" })
+	public String parserMethod(@RequestBody String a) {
+		return a;
+	}
 }
