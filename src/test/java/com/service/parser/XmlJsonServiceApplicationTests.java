fdd8c953b011a8691850692222d45a5a93338d53
Arjun <arjunsekar1991@gmail.com>
2018-04-24 01:14:20 +0530

    test methos fix
    test methos fix

--- src/test/java/com/service/parser/XmlJsonServiceApplicationTests.java
+++ src/test/java/com/service/parser/XmlJsonServiceApplicationTests.java
@@ -3,10 +3,14 @@ package com.service.parser;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.service.application.XmlJsonServiceApplication;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes=XmlJsonServiceApplication.class)
 public class XmlJsonServiceApplicationTests {
 
 	@Test
